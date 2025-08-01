f21ff175aee818a07cdc53d89498ad8c07e76e1d
Prasoon <142042273+PrasoonCr7@users.noreply.github.com>
2025-08-01 11:22:25 +0530

    Update phase1.py

--- phase1.py
+++ phase1.py
@@ -2,42 +2,7 @@
 # 2. Streamlit 
 # 3. Usuage from the UI
 
-
-# # PHASE 1
-
-# # 1. Chatbot
-# # 2. Streamlit 
-# # 3. Usuage from the UI
-
-# # # Import a streamlit library (Frontend)
-# import streamlit as st
-
-# # Title of the app
-# st.title("RAG Chatbot !")
-
-# # Initialize message history
-# if 'messages' not in st.session_state:
-#     st.session_state.messages = []
-
-# # Display previous messages
-# for message in st.session_state.messages:
-#     st.chat_message(message['role']).markdown(message['content'])
-
-# # Take user input
-# prompt = st.chat_input("Pass your prompt here!")
-
-# # If user submits a prompt
-# if prompt:
-#     # Append and display user message
-#     st.session_state.messages.append({'role': 'user', 'content': prompt})
-#     st.chat_message("user").markdown(prompt)
-
-#     # Process and display assistant response
-#     response = "I am your Assistant"
-#     st.session_state.messages.append({'role': 'assistant', 'content': response})
-#     st.chat_message("assistant").markdown(response)
-
-
+# Phase 2 have all the coding , this is just part 1 one RAG Chatbot
 
 # # Phase 1 imports
 import streamlit as st
@@ -49,7 +14,7 @@ from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
 
 # Set the API key securely (temporary, better via .env or Streamlit secrets)
-#os.environ["GROQ_API_KEY"] = "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"
+os.environ["GROQ_API_KEY"] = "Enter Groq API Key"
 
 # Title of the app
 st.title("RAG Chatbot !")
